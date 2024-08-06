2f17ef032f1b0a28d334a7807f04fdf7b301bc3a
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-08-06 09:42:55 +0200

    commit

--- src/routes/identities_kyc_checks/identities_kyc_checks.controller.ts
+++ src/routes/identities_kyc_checks/identities_kyc_checks.controller.ts
@@ -13,23 +13,23 @@ import { IdentitiesKycChecksService } from './identities_kyc_checks.service';
 @ApiTags('identity_kyc_checks')
 @Controller('identity_kyc_checks')
 export class IdentitiesKycChecksController {
-    constructor(private readonly identitiesKycChecksService: IdentitiesKycChecksService) {}
+  constructor(private readonly identitiesKycChecksService: IdentitiesKycChecksService) { }
 
-    @UseGuards(SupabaseGuard)
-    @Post()
-    @ApiOperation({ summary: 'Create a new identity kyc check' })
-    @ApiResponse({ status: 201, description: 'Identity Kyc Check created successfully' })
-    @ApiResponse({ status: 400, description: 'Invalid input' })
-    async create(@Request() req) {
-      return await this.identitiesKycChecksService.create(req);
-    }
+  @UseGuards(SupabaseGuard)
+  @Post()
+  @ApiOperation({ summary: 'Create a new identity kyc check' })
+  @ApiResponse({ status: 201, description: 'Identity Kyc Check created successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  async create(@Request() req) {
+    return await this.identitiesKycChecksService.create(req);
+  }
 
-    @UseGuards(SupabaseGuard)
-    @Get(':id')
-    @ApiOperation({ summary: 'Get identity kyc checks by ID' })
-    @ApiResponse({ status: 200, description: 'Identity Kyc Check found' })
-    @ApiResponse({ status: 404, description: 'Identity not found' })
-    async getById(@Request() req, @Param('id') id: string) {
-      return await this.identitiesKycChecksService.getById(id);
-    }
+  @UseGuards(SupabaseGuard)
+  @Get(':id')
+  @ApiOperation({ summary: 'Get identity kyc checks by ID' })
+  @ApiResponse({ status: 200, description: 'Identity Kyc Check found' })
+  @ApiResponse({ status: 404, description: 'Identity not found' })
+  async getById(@Request() req, @Param('id') id: string) {
+    return await this.identitiesKycChecksService.getById(id);
+  }
 }
--- src/routes/identities_kyc_checks/identities_kyc_checks.service.ts
+++ src/routes/identities_kyc_checks/identities_kyc_checks.service.ts
@@ -1,87 +1,82 @@
-import { generateTimestamp } from '@/common/helpers/utils';
 import { SUPABASE_CLIENT } from '@/providers/supabase.providers';
 import {
-  HttpException,
-  HttpStatus,
-  Inject,
-  Injectable,
+	HttpException,
+	HttpStatus,
+	Inject,
+	Injectable,
 } from '@nestjs/common';
 import { SupabaseClient } from '@supabase/supabase-js';
-import { identity } from 'rxjs';
 
 
 @Injectable()
 export class IdentitiesKycChecksService {
-    constructor(
-        @Inject(SUPABASE_CLIENT) private readonly supabase: SupabaseClient,
-    ) { }
+	constructor(
+		@Inject(SUPABASE_CLIENT) private readonly supabase: SupabaseClient,
+	) { }
 
-    async create(req: any) {
-        const { body: identities_kyc_checks, user } = req;
-        if (!identities_kyc_checks) throw new HttpException(
-            "Missing body",
-            HttpStatus.FORBIDDEN
-        );
+	async create(req: any) {
+		const { body: identities_kyc_checks, user } = req;
+		if (!identities_kyc_checks) throw new HttpException(
+			"Missing body",
+			HttpStatus.FORBIDDEN
+		);
 
-        const { identity_id } = identities_kyc_checks;
-        if (!identity_id) throw new HttpException(
-            "Missing identity_id",
-            HttpStatus.FORBIDDEN
-        );
-        try {
-            const { data, error} = await this.supabase
-            .from('identity_kyc_checks')
-            .insert({
-                ...identities_kyc_checks,
-                identity_id
-        })
-        .select()
-        .single();
+		const { identity_id } = identities_kyc_checks;
+		if (!identity_id) throw new HttpException(
+			"Missing identity_id",
+			HttpStatus.FORBIDDEN
+		);
+		try {
+			const { data, error } = await this.supabase
+				.from('identity_kyc_checks')
+				.insert(identities_kyc_checks)
+				.select()
+				.single();
 
-        if (error) throw new HttpException(
-            error.message,
-            HttpStatus.FORBIDDEN
-          );
-    
-          return data;
-        } catch (error) {
-            throw new HttpException(
-                {
-                    status: error.status,
-                    error: 'Failed to create identity kyc checks',
-                    message: error.message
-                },
-                HttpStatus.FORBIDDEN
-            );
-        }
-    }
+			if (error) throw new HttpException(
+				error.message,
+				HttpStatus.FORBIDDEN
+			);
 
-    async getById(paramId: string) {
-        try {
-            const { data, error } = await this.supabase
-            .from('identity_kyc_checks')
-            .select('*')
-            .eq('id', paramId)
-            .select()
-            .single();
+			return data;
+		} catch (error) {
+			throw new HttpException(
+				{
+					status: error.status,
+					error: 'Failed to create identity kyc checks',
+					message: error.message
+				},
+				HttpStatus.FORBIDDEN
+			);
+		}
+	}
 
-            if (error) throw new HttpException(
-                error.message,
-                HttpStatus.FORBIDDEN
-              );
-        
-              return data;
-        } catch (error) {
-            throw new HttpException(
-                {
-                    status: error.status,
-                    error: 'Failed to get identity kyc data by id',
-                    message: error.message
-                },
-                HttpStatus.FORBIDDEN
-            );
-        }
-    }
+	async getById(paramId: string) {
+		try {
+			const { data, error } = await this.supabase
+				.from('identity_kyc_checks')
+				.select('*')
+				.eq('id', paramId)
+				.select()
+				.single();
+
+			if (error) throw new HttpException(
+				error.message,
+				HttpStatus.FORBIDDEN
+			);
+
+			return data;
+		} catch (error) {
+			throw new HttpException(
+				{
+					status: error.status,
+					error: 'Failed to get identity kyc data by id',
+					message: error.message
+				},
+				HttpStatus.FORBIDDEN
+			);
+		}
+	}
 
 
 
