3d54c5bb4428258f88d801fcccd84ff58a79b148
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-08-07 11:04:54 +0200

    commit

--- src/routes/bank_accounts/bank_accounts.service.ts
+++ src/routes/bank_accounts/bank_accounts.service.ts
@@ -90,7 +90,7 @@ export class BankaccountsService {
     try {
       const { data, error } = await this.supabase
         .from('bank_accounts')
-        .select('*')
+        .select('*, entities(*)')
         .eq('id', paramId)
         .single();
 
