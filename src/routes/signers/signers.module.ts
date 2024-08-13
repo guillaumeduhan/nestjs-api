3fc38e34e1324b9accbd4179cd554be8e71b8d1f
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-08-13 12:19:30 +0200

    commit

--- src/routes/signers/signers.module.ts
+++ src/routes/signers/signers.module.ts
@@ -1,12 +1,13 @@
 import { SupabaseModule } from '@/auth/supabase/supabase.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { IdentitiesService } from '../identities/identities.service';
 import { SignersController } from './signers.controller';
 import { SignersService } from './signers.service';
 
 @Module({
   imports: [SupabaseModule, ConfigModule],
   controllers: [SignersController],
-  providers: [SignersService]
+  providers: [SignersService, IdentitiesService]
 })
 export class SignersModule { }
